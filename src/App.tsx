b510effca4afb64c18d12b338b58df178950bf3f
Silly_coder <maityp394@gmail.com>
2025-03-09 23:42:00 +0530

    Initail commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <Button variant={"destructive"}>hello pritam</Button>
+    </>
+  );
+}
+
+export default App;
